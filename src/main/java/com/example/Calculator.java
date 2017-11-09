29173e99665e1d8c3fb318108bf74f5dcb4e7ee4
Ritesh Tyagi <hi2tyagi@yahoo.com>
2017-11-09 13:46:46 +0530

    initial code

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.concurrent.TimeUnit;
+
+public class Calculator {
+
+	public int sum(int valOne, int valTwo) {
+		return valOne + valTwo;
+	}
+
+	public int divide(int valOne, int valTwo) {
+		// 200 DB loc
+		/*try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+		}*/
+		return valOne / valTwo;
+	}
+
+}
